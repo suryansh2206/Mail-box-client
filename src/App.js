1f309ef76db86e0a4140f28fbfd8b90fd07d751a
suryansh2206 <suryansh804.sg@gmail.com>
2023-05-27 18:11:15 +0530

    reset password

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import SignUp from "./Components/Auth/SignUp";
 import Login from "./Components/Auth/Login";
 import HomePage from "./Components/Screens/HomePage";
+import ResetPassword from "./Components/Auth/ResetPassword";
 
 const App = () => {
   const isAuth = localStorage.getItem("token");
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
         </>
       )}
+      <Route path="/resetpassword" element={<ResetPassword />} />
     </Routes>
   );
 };
---
+++ src/Components/Auth/ResetPassword.js
@@ -0,0 +1,58 @@
+import { useRef, useState } from "react";
+import classes from './ResetPassword.module.css';
+
+const ResetPassword = () => {
+  const emailInputRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetHandler = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const email = emailInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCY-VGJzQO4PuIAWLAzUqOd4c2XvpMOQFs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "PASSWORD_RESET",
+          email: email,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        res.json().then((data) => console.log(data));
+        alert("Reset password link sent");
+      } else {
+        return res.json().then((data) => {
+          alert(data.error.message);
+        });
+      }
+    });
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.card}>
+        <span className={classes.cardtitle}>
+          Enter the email with which you have registered
+        </span>
+        <div className={classes.cardform}>
+          <input placeholder="Email" type="text" required ref={emailInputRef} />
+          {!isLoading && (
+            <button className={classes.signup} onClick={resetHandler}>
+              Send Link
+            </button>
+          )}
+          {isLoading && <p>Sending link Please wait</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
